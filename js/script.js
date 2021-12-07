99dd5425fbe43b94563767668ec6ed47691c378f
ezekielswanson <swansonezekiel@gmail.com>
2021-12-06 20:11:13 -0600

    Updated js file.

--- js/script.js
+++ js/script.js
@@ -5,17 +5,28 @@ Treehouse Techdegree: Data Pagination and Filtering
 
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab
-   Reach out in your Slack community if you have questions
-*/
-
-
-
-/*
 Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+This function creates and inserts the elements needed to display a "page" of nine students
 */
+function showPage (list, page) {
+   // create two variables which will represent the index for the first and last student on the page
+   startIndex = (page parameter * items per page) - items per page;
+   endIndex = page parameter * items perp ge;
+
+  // select the element with a class of `student-list` and assign it to a variable
+
+
+  // set the innerHTML property of the variable you just created to an empty string
+
+  // loop over the length of the `list` parameter
+    // inside the loop create a conditional to display the proper students
+      // inside the conditional:
+        // create the elements needed to display the student information
+        // insert the above elements
+
+    
+
+};
 
 
 
