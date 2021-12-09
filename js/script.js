6f142a772474eda6c0ba1153dd12f0a9b51eedbf
ezekielswanson <swansonezekiel@gmail.com>
2021-12-08 19:44:44 -0600

    Updated js file.

--- js/script.js
+++ js/script.js
@@ -10,15 +10,29 @@ This function creates and inserts the elements needed to display a "page" of nin
 */
 function showPage (list, page) {
    // create two variables which will represent the index for the first and last student on the page
-   startIndex = (page parameter * items per page) - items per page;
-   endIndex = page parameter * items perp ge;
+   const startIndex = (page parameter * items per page) - items per page;
+   const endIndex = page parameter * items per page;
 
   // select the element with a class of `student-list` and assign it to a variable
+  const studentList = document.getElementsByClassName('student-list');
 
 
   // set the innerHTML property of the variable you just created to an empty string
+  studentList.innerHTML = '';
+
 
   // loop over the length of the `list` parameter
+  for (let i=0; list.length; i++) {
+
+      if( i >= startIndex && i < endIndex) {
+        const h3 = documnet.createElement(h3);
+        const 
+
+        studentItem = ${}
+
+      }
+
+  }
     // inside the loop create a conditional to display the proper students
       // inside the conditional:
         // create the elements needed to display the student information
