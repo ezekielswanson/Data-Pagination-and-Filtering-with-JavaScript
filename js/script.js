b88570b4cc3a2a73f8696a6a68170eec727b237d
ezekielswanson <swansonezekiel@gmail.com>
2021-12-10 19:50:31 -0600

    Updated js file.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 Treehouse Techdegree: Data Pagination and Filtering
 */
 
-
+/* Global Variables */
+const itemsPerPage = 9;
 
 /*
 Create the `showPage` function
@@ -12,30 +13,40 @@ List parameter represents array of student data.
 Page parameter reprsents the page number to be displayed.
 */
 function showPage (list, page) {
-   const startIndex = (page parameter * items per page) - items per page;
-   const endIndex = page parameter * items per page;
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
    const studentList = document.getElementsByClassName('student-list');
 
    studentList.innerHTML = '';
 
+
    for (let i=0; list.length; i++) {
       if ( i >= startIndex && i < endIndex) {
-            const studentItem = 
-            `<li class="student-item cf">
-            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+
+      const studentItem = 
+      
+      `<li class="student-item cf">
+      <div class="student-details">
+            <img class="avatar" src=${list[i].picture.thumbnail} alt='Profile Picture'>
             <h3>${list[i].name.first} ${list[i].name.last}</h3>
-            
-            
-            
-            `
-   
+            <span class="email">${list[i].email}</span>
+      </div>
+      <div>
+      <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+      </li>`;
 
-      }
+      studentList.insertAdjacentHTML('beforeend', studentDisplay);
 
-  }
-
-
+      };
+  };
+  showPage(data,1);
 };
+
+
+
+
+
 
 
 
@@ -47,3 +58,5 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+
+
